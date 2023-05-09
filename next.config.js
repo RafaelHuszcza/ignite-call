b81cef8c04a0155189467788173e099a9cb4bd44
RafaelHuszcza <rafaelhuszcza@gmail.com>
2023-05-09 00:39:58 -0300

    tudo

--- next.config.js
+++ next.config.js
@@ -1,22 +1,8 @@
 /** @type {import('next').NextConfig} */
 
-const isGithubActions = process.env.GITHUB_ACTIONS || false
-
-let assetPrefix = ''
-let basePath = '/'
-
-if (isGithubActions) {
-  // trim off `<owner>/`
-  const repo = process.env.GITHUB_REPOSITORY.replace(/.*?\//, '')
-  assetPrefix = `/${repo}/`
-  basePath = `/${repo}`
-}
-
 const nextConfig = {
   reactStrictMode: true,
   pageExtensions: ['page.tsx', 'api.ts', 'api.tsx'],
-  assetPrefix,
-  basePath,
 }
 
 module.exports = nextConfig
